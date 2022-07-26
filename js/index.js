21367456ba288e031001174586312a8c58db427c
vigehi <vigehi2017@gmail.com>
2022-07-27 00:58:18 +0300

    update on javscript menu

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
+const navMenu = document.querySelector('.navMenu');
+const navLink = document.querySelectorAll('.navLink');
+function mobileMenu() {
+  hamburgerMenu.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
+hamburgerMenu.addEventListener('click', mobileMenu);
+
+function closeMenu() {
+  hamburgerMenu.classList.remove('active');
+  navMenu.classList.remove('active');
+}
+
+navLink.forEach((n) => n.addEventListener('click', closeMenu));
